6d87ca350f6f18fbbdb1cc0583c84e89499dd306
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-13 16:08:31 -0600

    Updating an item using a from in SwiftData

---
+++ ToDoAppSD/UpdateToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateToDoView.swift
+//  ToDoAppSD
+//
+//  Created by Mitch Andrade on 6/13/23.
+//
+
+import SwiftUI
+
+struct UpdateToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateToDoView()
+}
