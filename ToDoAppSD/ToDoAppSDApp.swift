124a8f5c3ba07461347317d2a9b2a12b97ff0e12
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-13 07:26:03 -0600

    Initial Commit

---
+++ ToDoAppSD/ToDoAppSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSDApp.swift
+//  ToDoAppSD
+//
+//  Created by Mitch Andrade on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
