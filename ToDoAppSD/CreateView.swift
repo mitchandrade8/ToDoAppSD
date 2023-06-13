e9269ec8cd407a621aecf50a883d1041262c9e62
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-13 10:38:13 -0600

    Going over SwiftData, new macros and all other changes with beta xocde 15.

---
+++ ToDoAppSD/CreateView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateView.swift
+//  ToDoAppSD
+//
+//  Created by Mitch Andrade on 6/13/23.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateView()
+}
