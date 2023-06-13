08b877d70b38bf011a43da1184efca4e514901d4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-13 07:30:10 -0600

    -- Start - Practice Using SwiftData with small app

---
+++ ToDoAppSD/ToDoItem.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoItem.swift
+//  ToDoAppSD
+//
+//  Created by Mitch Andrade on 6/13/23.
+//
+
+import Foundation
+
+final class ToDoItem {
+    var title: String
+    var timestamp: Date
+    var isCritical: Bool
+    var isCompleted: Bool
+    
+    init(title: String = "",
+         timestamp: Date = .now,
+         isCritical: Bool = false,
+         isCompleted: Bool = false) {
+        self.title = title
+        self.timestamp = timestamp
+        self.isCritical = isCritical
+        self.isCompleted = isCompleted
+    }
+}
