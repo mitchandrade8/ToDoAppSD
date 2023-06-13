c8f406e605ee0107cf621982ed83e0cf9d8bc0ec
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-13 15:47:16 -0600

    Fetch data in SwiftData using Query

--- ToDoAppSD/ContentView.swift
+++ ToDoAppSD/ContentView.swift
@@ -6,24 +6,67 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
     @State private var showCreate = false
+    @Query private var items: [ToDoItem]
     
     var body: some View {
         
         NavigationStack {
-            Text("What it do baby")
-                .toolbar {
-                    ToolbarItem {
-                        Button(action: {
-                            showCreate.toggle()
-                        }, label: {
-                            Label("Add Item", systemImage: "plus")
-                        })
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            
+                            if item.isCritical {
+                                Image(systemName: "exclamationmark.3")
+                                    .symbolVariant(.fill)
+                                    .foregroundColor(.red)
+                                    .font(.largeTitle)
+                                    .bold()
+                            }
+                            
+                            Text(item.title)
+                                .font(.largeTitle)
+                                .bold()
+                            
+                            Text("\(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .shortened))")
+                                .font(.callout)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .symbolVariant(.circle.fill)
+                                .foregroundStyle(item.isCompleted ? .green : .gray)
+                                .font(.largeTitle)
+                        }
+                        .buttonStyle(.plain)
                     }
                 }
+            }
+            .navigationTitle("My focuses")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        showCreate.toggle()
+                    }, label: {
+                        Label("Add Item", systemImage: "plus")
+                    })
+                }
+            }
+            .sheet(isPresented: $showCreate, content: {
+                NavigationStack {
+                    CreateToDoView()
+                }
+                .presentationDetents([.medium])
+            })
         }
     }
 }
