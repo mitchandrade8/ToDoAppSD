a1e311d9c90857d11e0279aa15bb76aab076642c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-19 17:19:51 -0600

    Adding categories to our todo , overview of SwiftData relationships

---
+++ ToDoAppSD/CreateCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateCategoryView.swift
+//  ToDoAppSD
+//
+//  Created by Mitch Andrade on 6/19/23.
+//
+
+import SwiftUI
+
+struct CreateCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateCategoryView()
+}
